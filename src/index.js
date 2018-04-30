0826912f5b4529615ec20e1dbc75f003501e0dc0
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-04-29 19:34:48 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ else if(window.location.hash.indexOf("projects")!=-1){
   var proj=["WebOS", "/projects/webos"];
   var projects="";
   for(var i=0;i<proj.length/2;i++){projects+="<a href='"+proj[i+1]+"'>"+proj[i]+"</a><br />"}
-  projects+="<br /><a href='https://github.com/KentoNishi/kentonishi.github.io/tree/master/projects'>All Projects</a>";
+  projects+="<a href='https://github.com/KentoNishi/kentonishi.github.io/tree/master/projects'>All Projects</a>";
   dir("projects", "Projects", projects);
 }
 function dir(name, dir, desc){
