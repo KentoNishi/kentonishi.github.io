b6f0a64b4dbe3022fe00963687b77999b363ddf7
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-04-30 23:30:23 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,21 +41,42 @@ function getCookie(cname) {
     return "";
 }
 //fullscreen api
-  var videoElement = document.getElementsByClassName("ui")[0];
-    
-  function toggleFullScreen() {
-    if (!document.mozFullScreen && !document.webkitFullScreen) {
-      if (videoElement.mozRequestFullScreen) {
-        videoElement.mozRequestFullScreen();
-      } else {
-        videoElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-      }
-    } else {
-      if (document.mozCancelFullScreen) {
+function isFullScreen()
+{
+    return (document.fullScreenElement && document.fullScreenElement !== null)
+         || document.mozFullScreen
+         || document.webkitIsFullScreen;
+}
+
+
+function requestFullScreen(element)
+{
+    if (element.requestFullscreen)
+        element.requestFullscreen();
+    else if (element.msRequestFullscreen)
+        element.msRequestFullscreen();
+    else if (element.mozRequestFullScreen)
+        element.mozRequestFullScreen();
+    else if (element.webkitRequestFullscreen)
+        element.webkitRequestFullscreen();
+}
+
+function exitFullScreen()
+{
+    if (document.exitFullscreen)
+        document.exitFullscreen();
+    else if (document.msExitFullscreen)
+        document.msExitFullscreen();
+    else if (document.mozCancelFullScreen)
         document.mozCancelFullScreen();
-      } else {
-        document.webkitCancelFullScreen();
-      }
-    }
-  }
-  
+    else if (document.webkitExitFullscreen)
+        document.webkitExitFullscreen();
+}
+
+function toggleFullScreen(element)
+{
+    if (isFullScreen())
+        exitFullScreen();
+    else
+        requestFullScreen(element || document.documentElement);
+}
