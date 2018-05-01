fecd9c6d0fb4586222fb54c00d11ddefe369c943
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-01 00:24:10 -0700

    Update run.js

--- projects/webos/run/run.js
+++ projects/webos/run/run.js
@@ -1,9 +1,1 @@
-window.onload=function(){
-document.querySelectorAll("taskbar")[0].addEventListener("scroll", function(event) {
-  
-    var top = this.scrollY,
-        left = this.scrollX;
-    console.log(top+" - "+left);
-  
-}, false);
-}
+
