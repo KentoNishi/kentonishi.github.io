3ab5387e65f90b7b35c4228bb1d141d097e78aab
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-04-30 23:16:10 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,3 +40,17 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
