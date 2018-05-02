f9cf972487c7103ed4d7c0cd4b817e538fedf2f7
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-01 23:52:28 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -85,3 +85,21 @@ function toggleFullScreen(element)
     */
   requestFullScreen(element || document.documentElement);
 }
+function getDomain(url) {
+    var dom = "", v, step = 0;
+    for(var i=0,l=url.length; i<l; i++) {
+        v = url[i]; if(step == 0) {
+            //First, skip 0 to 5 characters ending in ':' (ex: 'https://')
+            if(i > 5) { i=-1; step=1; } else if(v == ':') { i+=2; step=1; }
+        } else if(step == 1) {
+            //Skip 0 or 4 characters 'www.'
+            //(Note: Doesn't work with www.com, but that domain isn't claimed anyway.)
+            if(v == 'w' && url[i+1] == 'w' && url[i+2] == 'w' && url[i+3] == '.') i+=4;
+            dom+=url[i]; step=2;
+        } else if(step == 2) {
+            //Stop at subpages, queries, and hashes.
+            if(v == '/' || v == '?' || v == '#') break; dom += v;
+        }
+    }
+    return dom;
+}
