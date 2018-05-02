4f5f879c71b8f1e91af227921445b54f1610b62e
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-01 20:45:35 -0700

    Update run.js

--- projects/webos/run/run.js
+++ projects/webos/run/run.js
@@ -1,1 +1,64 @@
+var node = document.getElementsByClassName("home")[0];
+var longpress = false;
+var presstimer = null;
+var longtarget = null;
 
+var cancel = function(e) {
+    if (presstimer !== null) {
+        clearTimeout(presstimer);
+        presstimer = null;
+    }
+    
+    this.classList.remove("longpress");
+};
+var clickCount=0;
+var click = function(e) {
+    if (presstimer !== null) {
+        clearTimeout(presstimer);
+        presstimer = null;
+    }
+    
+    this.classList.remove("longpress");
+    
+    if (longpress) {
+        return false;
+    }
+    clickCount++;
+    if (clickCount === 1) {
+        singleClickTimer = setTimeout(function() {
+            clickCount = 0;
+            alert("click");
+        }, 250);
+    } else if (clickCount === 2) {
+        clearTimeout(singleClickTimer);
+        clickCount = 0;
+        alert("doubleclick");
+    }
+};
+
+var start = function(e) {
+    console.log(e);
+    
+    if (e.type === "click" && e.button !== 0) {
+        return;
+    }
+    
+    longpress = false;
+    
+    this.classList.add("longpress");
+    
+    presstimer = setTimeout(function() {
+        alert("long click");
+        longpress = true;
+    }, 250);
+    
+    return false;
+};
+
+node.addEventListener("mousedown", start);
+node.addEventListener("touchstart", start);
+node.addEventListener("click", click);
+node.addEventListener("mouseout", cancel);
+node.addEventListener("touchend", cancel);
+node.addEventListener("touchleave", cancel);
+node.addEventListener("touchcancel", cancel);
