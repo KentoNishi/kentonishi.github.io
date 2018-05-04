1dc1d9834f9f4dbc74765a2a113d58a85c7d3213
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-03 22:46:37 -0700

    Update run.js

--- projects/webos/run/run.js
+++ projects/webos/run/run.js
@@ -4,7 +4,7 @@ $('.gesturearea').on("touchmove", gesture);
 recalibrate();
 }
 function recalibrate(){
-setTimeout(function(){document.querySelectorAll(".gesturearea")[0].scrollTop=(document.querySelectorAll(".gesturearea")[0].clientHeight/2)+document.querySelectorAll(".gesturebar")[0].clientHeight/2}, 100);
+setTimeout(function(){document.querySelectorAll(".gesturearea")[0].scrollTop=(document.querySelectorAll(".gesturearea")[0].clientHeight/2)+document.querySelectorAll(".gesturebar")[0].clientHeight/2}, 1);
 }
 function gesture(){
     if(document.querySelectorAll(".gesturearea")[0].scrollTop+1<(document.querySelectorAll(".gesturearea")[0].clientHeight/2)+document.querySelectorAll(".gesturebar")[0].clientHeight/2){
