6a596c5ccc2e7fc6b079788718036414122d73a5
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-03 22:14:56 -0700

    Update run.js

--- projects/webos/run/run.js
+++ projects/webos/run/run.js
@@ -1,8 +1,15 @@
 window.onload=function(){
-//$('.gesturearea').on("scroll", gesture);
-//$('.gesturearea').on("touchmove", gesture);
+$('.gesturearea').on("scroll", gesture);
+$('.gesturearea').on("touchmove", gesture);
 }
 function gesture(){
+    if(document.querySelectorAll(".gesturearea")[0].scrollTop==0){
+        action("apps");
+    }
+    else if(document.querySelectorAll(".gesturearea")[0].scrollTop-document.querySelectorAll(".gesturearea")[0].clientHeight==document.querySelectorAll(".gesturescroller")[0].clientHeight){
+        action("home");
+    }
+    
 }
 function action(action){
     document.getElementById("action").innerHTML=(action);
