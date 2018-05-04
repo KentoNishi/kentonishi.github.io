fd61f30772f1fe9d5b1ccc7d198e7154b593a6f4
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-03 21:39:53 -0700

    Update run.js

--- projects/webos/run/run.js
+++ projects/webos/run/run.js
@@ -2,4 +2,13 @@ window.onload=function(){
 function action(action){
     console.log(action);
 }
+document.querySelectorAll('.gesturearea')[0].on("scroll", gesture);
+document.querySelectorAll('.gesturearea')[0].on("touchmove", gesture);
+}
+function gesture(){
+    if(document.querySelectorAll('.gesturearea')[0].scrollTop<1){
+        action("apps");
+       }else{
+           action("home");
+       }
 }
