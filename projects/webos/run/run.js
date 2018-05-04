5f747bed5314f981d1a10a9843b348781323467f
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-03 22:29:49 -0700

    Update run.js

--- projects/webos/run/run.js
+++ projects/webos/run/run.js
@@ -1,8 +1,10 @@
 window.onload=function(){
 $('.gesturearea').on("scroll", gesture);
 $('.gesturearea').on("touchmove", gesture);
-document.querySelectorAll(".gesturearea")[0].scrollTop=(document.querySelectorAll(".gesturearea")[0].clientHeight/2)+document.querySelectorAll(".gesturebar")[0].clientHeight/2;
-
+recalibrate();
+}
+function recalibrate(){
+setTimeout(function(){document.querySelectorAll(".gesturearea")[0].scrollTop=(document.querySelectorAll(".gesturearea")[0].clientHeight/2)+document.querySelectorAll(".gesturebar")[0].clientHeight/2}, 100);
 }
 function gesture(){
     if(document.querySelectorAll(".gesturearea")[0].scrollTop+1<(document.querySelectorAll(".gesturearea")[0].clientHeight/2)+document.querySelectorAll(".gesturebar")[0].clientHeight/2){
@@ -13,6 +15,6 @@ function gesture(){
     }
 }
 function action(action){
-    document.querySelectorAll(".gesturearea")[0].scrollTop=(document.querySelectorAll(".gesturearea")[0].clientHeight/2)+document.querySelectorAll(".gesturebar")[0].clientHeight/2;
+    recalibrate();
     document.getElementById("action").innerHTML=(action);
 }
