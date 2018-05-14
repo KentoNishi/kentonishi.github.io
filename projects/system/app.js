44cb7beaed20297aa6931062e72dafff6b49cc45
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-13 22:34:11 -0700

    Create app.js

---
+++ projects/system/app.js
@@ -0,0 +1,63 @@
+  //https://firebase.google.com/docs/auth/web/google-signin?authuser=0
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB5XNbaaKee9GqQ74FjHPHam055_FqrVf4",
+    authDomain: "kento-nishi-gi-1525841644617.firebaseapp.com",
+    databaseURL: "https://kento-nishi-gi-1525841644617.firebaseio.com",
+    projectId: "kento-nishi---gi-1525841644617",
+    storageBucket: "kento-nishi---gi-1525841644617.appspot.com",
+    messagingSenderId: "939148943087"
+  };
+
+  firebase.initializeApp(config);
+  var storage = firebase.storage();
+  var storageRef = storage.ref();
+  
+  var uid="";
+  
+  function put(title, content){
+    var ref=storageRef.child("user/"+uid+'/'+title+".txt");
+    var input = content;
+    var file = new Blob([input], {
+      type: 'text/plain'
+    });
+    ref.put(file).then(function(snapshot) {
+//      console.log('Uploaded a blob or file!');
+    });
+  }
+  
+var response="";
+  function get(title){
+    response="";
+    storageRef.child('user/'+uid+'/'+title+'.txt').getDownloadURL().then(function(url) {
+      $.get(url, function(data, status){
+          response=data;
+      });
+    });/*.catch(function(error) {
+      // Handle any errors
+    });*/
+//    return response;
+  }
+  
+  function login(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      uid=result.user.uid;
+//      console.log(token+" - "+user);
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+//      console.log(errorCode+" - "+errorMessage+" - "+email+" - "+credential);
+      // ...
+    });
+  }
