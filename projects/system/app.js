3750b40e50fd54d80183142fa155d1d33067e5a1
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-13 23:06:10 -0700

    Update app.js

--- projects/system/app.js
+++ projects/system/app.js
@@ -1,5 +1,6 @@
   //https://firebase.google.com/docs/auth/web/google-signin?authuser=0
   // Initialize Firebase
+
   var config = {
     apiKey: "AIzaSyB5XNbaaKee9GqQ74FjHPHam055_FqrVf4",
     authDomain: "kento-nishi-gi-1525841644617.firebaseapp.com",
