2cb54869cd2517888fa44eebde0e2be6b75daa4e
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-16 01:34:45 -0700

    Update app.js

--- projects/database/app.js
+++ projects/database/app.js
@@ -1,1 +1,37 @@
-
+ //https://firebase.google.com/docs/auth/web/google-signin?authuser=0
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB5XNbaaKee9GqQ74FjHPHam055_FqrVf4",
+    authDomain: "kento-nishi-gi-1525841644617.firebaseapp.com",
+    databaseURL: "https://kento-nishi-gi-1525841644617.firebaseio.com",
+    projectId: "kento-nishi---gi-1525841644617",
+    storageBucket: "kento-nishi---gi-1525841644617.appspot.com",
+    messagingSenderId: "939148943087"
+  };
+  firebase.initializeApp(config);
+  var storage = firebase.storage();
+  var storageRef = storage.ref();
+  var database = firebase.database();
+  function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+  function login(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var uid=result.user.uid;
+      var name=result.user.displayName;
+      var pic=result.user.photoURL;
+      var email=result.user.email;
+      writeUserData(uid,name,email,pic);
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+    });
+  }
