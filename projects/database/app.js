ba164d8cd26ee801845ee3ca1ad67faef7be0f54
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-18 18:53:34 -0700

    Update app.js

--- projects/database/app.js
+++ projects/database/app.js
@@ -63,10 +63,7 @@
       var pic=result.user.photoURL;
       var email=result.user.email;
       writeUserData(uid,name,email,pic);
-    });/*.catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-    });*/
+    }).catch(function(error) {
+      console.log("SIGN IN ERROR!");
+    });
   }
