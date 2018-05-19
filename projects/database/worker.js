3df63bd3d098343718e713763e580ebff2b06264
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-18 17:23:43 -0700

    Update worker.js

--- projects/database/worker.js
+++ projects/database/worker.js
@@ -17,17 +17,10 @@ self.addEventListener('install', function(event) {
   );
 });
 self.addEventListener('fetch', function(event) {
-
   console.log(event.request.url);
-
   event.respondWith(
-
   caches.match(event.request).then(function(response) {
-
-  return response || fetch(event.request);
-
+    return response || fetch(event.request);
   })
-
   );
-
 });
