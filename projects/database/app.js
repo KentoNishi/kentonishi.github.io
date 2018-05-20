8c0bdc9680f5a10a61e92f58721918d72e9567e0
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-20 16:24:08 -0700

    Update app.js

--- projects/database/app.js
+++ projects/database/app.js
@@ -14,6 +14,23 @@
   var storageRef = storage.ref();
   var database = firebase.database();
 
+  function notifyMe() {
+    if (Notification.permission !== "granted")
+      Notification.requestPermission();
+    else {
+      var notification = new Notification('Notification title', {
+        icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+        body: "Hey there! You've been notified!",
+      });
+
+      notification.onclick = function () {
+        window.open("http://stackoverflow.com/a/13328397/1269037");      
+      };
+
+    }
+
+  }
+
 /*
   var messaging = firebase.messaging();
   messaging.usePublicVapidKey("BCKtXl1aH0s1dSXEqoaXi9yAXckJusY1suWxPQPbiELn1z6DEN6hReNdUODWVTR2K4wQGdq-11dWc8x-TUeCKoo");
@@ -69,6 +86,16 @@
     }
     });
 //    messager();
+   
+    if (!Notification) {
+      alert('Desktop notifications not available in your browser. Try Chromium.'); 
+      return;
+    }
+
+    if (Notification.permission !== "granted"){
+      Notification.requestPermission();
+    }
+   
   }
   
   function writeUserData(userId, name, email, imageUrl) {
