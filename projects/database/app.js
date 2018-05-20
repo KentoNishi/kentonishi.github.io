887f50618593989174f4a29b795b230369fee8da
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-20 00:34:26 -0700

    Update app.js

--- projects/database/app.js
+++ projects/database/app.js
@@ -1,6 +1,5 @@
   document.body.onclick=function(){requestFullScreen(document.body);}
 
-  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB5XNbaaKee9GqQ74FjHPHam055_FqrVf4",
     authDomain: "kento-nishi-gi-1525841644617.firebaseapp.com",
