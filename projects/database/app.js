8597c280c271593b848e7f2520d0c0b8c131a12e
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-19 17:35:22 -0700

    Update app.js

--- projects/database/app.js
+++ projects/database/app.js
@@ -15,8 +15,6 @@
   var storage = firebase.storage();
   var storageRef = storage.ref();
   var database = firebase.database();
-  var messaging = firebase.messaging();
-  messaging.usePublicVapidKey("AIzaSyB5XNbaaKee9GqQ74FjHPHam055_FqrVf4");
   
   window.onload=function(){
     firebase.auth().onAuthStateChanged(function(user) {
@@ -28,56 +26,8 @@
       // No user is signed in.
     }
     });
-    
-    messaging.requestPermission().then(function() {
-      console.log('Notification permission granted.');
-      // TODO(developer): Retrieve an Instance ID token for use with FCM.
-      // ...
-    }).catch(function(err) {
-      console.log('Unable to get permission to notify.', err);
-    });
 
   }
-
-  // Get Instance ID token. Initially this makes a network call, once retrieved
-  // subsequent calls to getToken will return from cache.
-  messaging.getToken().then(function(currentToken) {
-    if (currentToken) {
-      sendTokenToServer(currentToken);
-      updateUIForPushEnabled(currentToken);
-    } else {
-      // Show permission request.
-      console.log('No Instance ID token available. Request permission to generate one.');
-      // Show permission UI.
-      updateUIForPushPermissionRequired();
-      setTokenSentToServer(false);
-    }
-  }).catch(function(err) {
-    console.log('An error occurred while retrieving token. ', err);
-    showToken('Error retrieving Instance ID token. ', err);
-    setTokenSentToServer(false);
-  });
-
-  // Callback fired if Instance ID token is updated.
-  messaging.onTokenRefresh(function() {
-    messaging.getToken().then(function(refreshedToken) {
-      console.log('Token refreshed.');
-      // Indicate that the new Instance ID token has not yet been sent to the
-      // app server.
-      setTokenSentToServer(false);
-      // Send Instance ID token to app server.
-      sendTokenToServer(refreshedToken);
-      // ...
-    }).catch(function(err) {
-      console.log('Unable to retrieve refreshed token ', err);
-      showToken('Unable to retrieve refreshed token ', err);
-    });
-  });
-
-  messaging.onMessage(function(payload) {
-   console.log('Message received. ', payload);
-  });
-
   
   function writeUserData(userId, name, email, imageUrl) {
     firebase.database().ref('users/' + userId).set({
