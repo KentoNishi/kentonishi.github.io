ba210bc32b8f740a009edc4b2914766fcc0b75ad
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-21 23:20:01 -0700

    Update app.js

--- projects/database/app.js
+++ projects/database/app.js
@@ -15,7 +15,6 @@
   var database = firebase.database();
 
 /*
-
   var messaging = firebase.messaging();
   messaging.usePublicVapidKey("BCKtXl1aH0s1dSXEqoaXi9yAXckJusY1suWxPQPbiELn1z6DEN6hReNdUODWVTR2K4wQGdq-11dWc8x-TUeCKoo");
   //init messaging
@@ -59,7 +58,6 @@
       });
     });
   }
-  
   */
 
   window.onload=function(){
