6cbbb4f65d73652a832f5f54f7e59f5838e79d33
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-23 01:02:34 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -1,3 +1,4 @@
-  if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://kentonishi.github.io/apps/gatherapp/worker.js').then(function() {console.log('Service Worker Registered');});
-  }
+//Service Worker
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('https://kentonishi.github.io/apps/gatherapp/worker.js').then(function() {console.log('Service Worker Registered');});
+}
