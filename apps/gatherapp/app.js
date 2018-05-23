21dfa0579d54ff3f99b1cbd6e46b6525a420457c
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-22 19:40:48 -0700

    Create app.js

---
+++ apps/gatherapp/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDofqiHzYbRz8J07H5Vp4_YGaX84kJ-7OQ",
+    authDomain: "gatherapp-513ab.firebaseapp.com",
+    databaseURL: "https://gatherapp-513ab.firebaseio.com",
+    projectId: "gatherapp-513ab",
+    storageBucket: "gatherapp-513ab.appspot.com",
+    messagingSenderId: "190580601957"
+  };
+  firebase.initializeApp(config);
