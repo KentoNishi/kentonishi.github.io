9b267ca67d3d6a686548a3b405961dc994d8cce9
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-23 01:14:20 -0700

    Delete app.js

--- apps/gatherapp/app.js
+++
@@ -1,4 +0,0 @@
-//Service Worker
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('https://kentonishi.github.io/apps/gatherapp/worker.js').then(function() {console.log('Service Worker Registered');});
-}
