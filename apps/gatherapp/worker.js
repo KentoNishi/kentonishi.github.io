b5c54aab432db02e64cae9628e4af8fa3f820c44
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-22 20:28:07 -0700

    Create worker.js

---
+++ apps/gatherapp/worker.js
@@ -0,0 +1,24 @@
+  var CACHE_NAME = 'cache';
+  var urlsToCache = [
+    '/apps/gatherapp/index.html',
+    '/apps/gatherapp/app.js'
+  ];
+
+  self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache.');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+  self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+    );
+  });
