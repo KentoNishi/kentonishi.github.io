fddde21165b8ae71395b9d4457c571dd3be0d61f
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-22 19:58:42 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -28,3 +28,28 @@
       console.log(error.message);
     });
   }
+
+  var CACHE_NAME = 'cache';
+  var urlsToCache = [
+    '/apps/gatherapp/index.html',
+    '/apps/gatherapp/app.js'
+  ];
+
+  self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache.');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+  self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+    );
+  });
