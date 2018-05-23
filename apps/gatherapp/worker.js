4739c8248c3161ef06540da36f66fe66888dfe4a
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-22 21:07:42 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -1,24 +1,25 @@
-  var CACHE_NAME = 'cache';
-  var urlsToCache = [
-    'https://kentonishi.github.io/apps/gatherapp/index.html'
-  ];
+var CACHE_NAME = 'cache';
+var urlsToCache = [
+  'https://kentonishi.github.io/app/gatherapp/app.js',
+  'https://kentonishi.github.io/app/gatherapp/',
+  'https://kentonishi.github.io/app/gatherapp/index.html'
+];
 
-  self.addEventListener('install', function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache.');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
-
-  self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-    event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-    );
-  });
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+  caches.match(event.request).then(function(response) {
+    return response || fetch(event.request);
+  })
+  );
+});
