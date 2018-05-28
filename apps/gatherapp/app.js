f6e368af317febe45992b810e27865d1b91432a8
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-27 23:39:31 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -50,7 +50,7 @@ function writeData(userId,data) {
 }
 
 function loadUser(name,email,pic,desc){
-  document.querySelectorAll(".body")[0].innerHTML='<div class="card"><h2>'+name+'</h2><img class="pic" alt="Profile Picture" src="'+pic+'"></img><br /><br />'+desc+'</div>';
+  document.querySelectorAll(".body")[0].innerHTML='<div class="card"><span style="font-size:5vh;">'+name+'</span><img class="pic" alt="Profile Picture" src="'+pic+'"></img><br /><br />'+desc+'</div>';
 }
 
 function readData(user){
