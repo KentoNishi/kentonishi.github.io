db7e04f033876c0022655fdd2f66dabff37e3cfd
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-27 21:50:32 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -2,6 +2,7 @@
 if('serviceWorker' in navigator) {
   navigator.serviceWorker.register('https://kentonishi.github.io/apps/gatherapp/worker.js').then(function() {console.log('Service Worker Registered');});
 }
+
 //FireBase
 var config = {
   apiKey: "AIzaSyB5XNbaaKee9GqQ74FjHPHam055_FqrVf4",
@@ -11,6 +12,7 @@ var config = {
   storageBucket: "kento-nishi---gi-1525841644617.appspot.com",
   messagingSenderId: "939148943087"
 };
+
 firebase.initializeApp(config);
 function action(act){
   console.log(act);
