fe64f5e700754f916c12b10e53c88f0a82d401e5
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-27 23:58:46 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -24,6 +24,7 @@ function action(act){
   }
 }
 var uid="";
+
 function login(){
   var provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then(function(result) {
