f36d262e03e77f3c9b25b9c40bde6e9345fa05f3
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-28 13:43:44 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -30,3 +30,21 @@ self.addEventListener('fetch', function(event) {
   })
   );
 });
+
+if(navigator.onLine){
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
+}
