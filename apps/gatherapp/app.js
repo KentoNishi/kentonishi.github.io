dbc13f96bbbebb5d56a4f80f3385dec504ebc4c9
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-27 22:33:05 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -5,12 +5,12 @@ if('serviceWorker' in navigator) {
 
 //Initialize FireBase
 var config = {
-  apiKey: "AIzaSyB5XNbaaKee9GqQ74FjHPHam055_FqrVf4",
-  authDomain: "kento-nishi-gi-1525841644617.firebaseapp.com",
-  databaseURL: "https://kento-nishi-gi-1525841644617.firebaseio.com",
-  projectId: "kento-nishi---gi-1525841644617",
-  storageBucket: "kento-nishi---gi-1525841644617.appspot.com",
-  messagingSenderId: "939148943087"
+  apiKey: "AIzaSyDpWZcmNnF0rmmYJOLgI0-cZJMIvvHngsY",
+  authDomain: "gatherapp-1906b.firebaseapp.com",
+  databaseURL: "https://gatherapp-1906b.firebaseio.com",
+  projectId: "gatherapp-1906b",
+  storageBucket: "",
+  messagingSenderId: "1038044491990"
 };
 firebase.initializeApp(config);
 
