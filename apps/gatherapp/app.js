ed0ccf290db80fac8e5c1a71c553805d2a2372e3
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-29 16:43:48 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -66,7 +66,7 @@ function loadUser(name,email,pic,desc){
 
 function getDesc(desc){
   console.log("desc: "+desc);
-  if(desc.replace(/ /g,"").toString().length<1){
+  if(desc.replace(/ /g,"").replace(/&nbsp;/g,"").toString().length<1){
     return '[Description Here]';
   }else{
     return desc;
