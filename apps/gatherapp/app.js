1b364d6d8185a76f2ebe48c21d77f0aa463be4fa
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-29 20:54:34 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -262,12 +262,11 @@ function action(act){
 }
 
 function auto_grow(element) {
-    element.style.height = "5px";
     element.style.height = (element.scrollHeight)+"px";
 }
 function enter(event){
   if(event.keyCode==13){
     document.querySelectorAll("textarea")[0].blur();
-    put(document.querySelectorAll("textarea")[0].value);
+    put(encodeURIComponent(document.querySelectorAll("textarea")[0].value));
   }
 }
