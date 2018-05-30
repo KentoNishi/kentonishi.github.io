a5a31b0ad376b82a9467d03f1e704824d9008d75
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-30 16:17:26 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -250,7 +250,7 @@ function writeUser(name,pic){
 //Put Data
 function put(info){
   console.clear();
-  if(info!=null){
+  if(info!=null&&uid!=null&&uid.length>1){
     if(info.split(",").length==1){
       info=",,,:"+info;
     }
@@ -276,7 +276,7 @@ function put(info){
 //Actions
 function action(act){
   console.log(act);
-  if(uid!=null||uid.length>1){
+  if(uid!=null&&uid.length>1){
     if(act=="menu"){
       var card="<div class='card'><span style='font-size:5vh'>"+name+"</span><br /><img class='pic' src='"+pic+"'></img><br /><a href='mailto:"+email+"'>@"+email.split("@")[0]+"</a><br /><textarea spellcheck='false' onkeyup='auto_grow(this)' style='text-align:center;font-size:4vh;' onkeypress='enter(event)' maxlength='144' >"+desc+"</textarea></div>";
       document.querySelectorAll(".body")[0].innerHTML=card;
