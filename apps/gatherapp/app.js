e1de35c149ba15463ae85e90c68d72625ae8c7d2
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-30 16:18:38 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -3,8 +3,6 @@ if('serviceWorker' in navigator) {
   navigator.serviceWorker.register('https://kentonishi.github.io/apps/gatherapp/worker.js').then(function() {console.log('Service Worker Registered');});
 }
 
-
-
 //Initialize FireBase
 var config = {
   apiKey: "AIzaSyDpWZcmNnF0rmmYJOLgI0-cZJMIvvHngsY",
