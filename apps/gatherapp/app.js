f794ee0d8401a8c2ef0a59188b6d3e9225f47a56
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-05-30 17:36:31 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -330,5 +330,19 @@ function newGroup(title){
 
 //ADD GROUP
 function add(now,title){
+  if(now==null){now="";}
   console.log(now+" "+title);
+  var ref=firebase.storage().ref().child("groups/"+title+".txt");
+  var string="";
+  if(now==null){
+   string = now+""+uid;
+  }else{
+   string = now+","+uid;
+  }
+  var file = new Blob([string], {
+      type: 'text/plain'
+  });
+  ref.put(file).then(function(snapshot) {
+    console.log('Reuploaded groupdata');
+  });
 }
