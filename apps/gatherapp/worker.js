dbe2f4f6d4a5a2db43cbbfb5689591ee6d57d39b
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-02 21:40:43 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -33,6 +33,7 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+//Delete Caches... ?
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
