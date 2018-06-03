566c9b84ac0e9174c7fabbba0cce342c7d07cb40
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-02 21:59:28 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -15,6 +15,7 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
@@ -25,11 +26,16 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
   event.respondWith(
-  caches.match(event.request).then(function(response) {
-    return response || fetch(event.request);
-  })
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
 
