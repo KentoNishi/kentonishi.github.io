f71708137c77fed04e4c16f691969d76ef0e8787
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-02 21:41:42 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -33,20 +33,3 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
-//Delete Caches... ?
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return true;
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
