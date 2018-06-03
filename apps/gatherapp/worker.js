23a358d198dcfd066b9015494da1c971340545ed
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-02 21:23:12 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -32,17 +32,3 @@ self.addEventListener('fetch', function(event) {
   })
   );
 });
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return true;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
