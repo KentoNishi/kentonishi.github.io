71b7f1b848f12b5d2b87e21956cde444694c8b12
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-06 17:29:45 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -1,19 +1,6 @@
 console.log('%c GatherApp ', 'background: #222; color: #bada55');
 //console.log("GatherApp by Kento Nishi. Created in 2018.");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Register Service Worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('https://kentonishi.github.io/apps/gatherapp/worker.js').then(function() {
