8f97bf37d7517b0d93cf75df0775f8e4f59f608f
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-06 17:10:41 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -182,11 +182,8 @@ function newGroup(title) {
         firebase.database().ref('users/'+ "groups/" + uid ).push().set({
             group: title
         }).then(function(){
-            firebase.database().ref('groups/'+title+"/users").set({
-            }).then(function(){
-                firebase.database().ref('groups/'+title+"/users").push().set({
-                    user: uid
-                });
+            firebase.database().ref('groups/'+title+"/users").push().set({
+                user: uid
             });
         });
     }else{
