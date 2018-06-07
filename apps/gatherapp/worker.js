b690511c96aabda71c835d088f513aa1e4c79dfb
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-06 19:00:11 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -18,7 +18,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
+        //'Opened cache
         return cache.addAll(urlsToCache);
       })
   );
