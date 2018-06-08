ce1d824b3c6aa4a256a98d411cdb74c42866bb28
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-08 13:10:01 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -260,6 +260,7 @@ function create(name){
     set("set","groups/"+key+"/info","group",name);
     set("update","groups/"+key+"/users",uid,true);
     set("update","groups/"+key+"/stats","popularity",0);
+    set("update","groups/"+key+"/stats","group",key);
     counter(key,"join");
     groups(uid);
 }
