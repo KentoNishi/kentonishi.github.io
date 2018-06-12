a472ab275959afdcc8dcf2176a13f2e1188e2538
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-12 15:04:41 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -415,7 +415,7 @@ function write(title,content,link,nav){
        body+='<br />';
        body+="<span>@"+encode(email.split("@")[0])+"</span>";
        body+='<br />';
-       body+="<span class='desc' contenteditable onkeyup='"+decodeURIComponent("set(%22update%22%2C%22users%2F%22%2Buid%2B%22%2Finfo%22%2C%22desc%22%2Cdecode(this.innerHTML))%3B")+"'>"+encode(desc||"[Description Here]").replace(/&amp;quot;/g,'"').replace(/&lt;br&gt;/g,'<br />')+"</span>";
+       body+="<span class='desc' contenteditable onkeyup='"+decodeURIComponent("set(%22update%22%2C%22users%2F%22%2Buid%2B%22%2Finfo%22%2C%22desc%22%2Cdecode(this.innerHTML))%3B")+"' onkeypress='function(event){if(event.keyCode==13){return false;}else{return event.keyCode;}}'>"+encode(desc||"[Description Here]").replace(/&amp;quot;/g,'"').replace(/&lt;br&gt;/g,'<br />')+"</span>";
        body+='<br />';
     }else{
        if(content!=""){
