7900e15b62c8f0b34c524a0dde183451be1d227b
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-12 18:18:30 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -15,6 +15,16 @@
     }
 })();
 
+
+
+
+
+
+
+
+
+
+
 //APIS AND BASIC FUNCTIONS
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('https://kentonishi.github.io/apps/gatherapp/worker.js').then(function() {
