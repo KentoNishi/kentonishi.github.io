8114521b28186405169cfdb98f4089d9934de49b
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-14 21:01:57 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -259,7 +259,7 @@ function find(title){
         firebase.database().ref("groups/").orderByChild("info/search").startAt(title.toLowerCase()).endAt(title.toLowerCase()+"\uf8ff").limitToFirst(25).once('value', snapshot => {
             reverseSnapshotOrder(snapshot).forEach(child => {
                 if(i==0){clear("body");}
-                write(child.val().info.group,child.val().info.desc,"group('"+child.key+"');",null,child.val().stats.popularity.toString()+" members");
+                write(child.val().info.group,child.val().info.desc,"group('"+child.key+"');",null,null,child.val().stats.popularity.toString()+" members");
                 i++;
             });
         });
@@ -336,7 +336,7 @@ function popularity(callback){
             if(child.val().stats.popularity>0){
                 if(i==0){clear("body");}
                 if(child.val().stats.popularity!=0){
-                    write(child.val().info.group,child.val().info.desc,"group('"+child.key+"');",null,child.val().stats.popularity.toString()+" members");
+                    write(child.val().info.group,child.val().info.desc,"group('"+child.key+"');",null,null,child.val().stats.popularity.toString()+" members");
                 }
                 i++;
             }
@@ -447,11 +447,11 @@ function write(title,content,link,nav,href,extra){
            body+='<br />';
            body+=encode(content).replace(/&amp;quot;/g,'"');
            body+='<br />';
-           if(extra!=null){
-               body+=encode(extra).replace(/&amp;quot;/g,'"');
-               body+='<br />';
-           }
        }
+    }
+    if(extra!=null){
+        body+=encode(extra).replace(/&amp;quot;/g,'"');
+        body+='<br />';
     }
     if(link!=null&&nav!=null){
         body+='<a href="'+link+'">'+encode(nav).replace(/&amp;quot;/g,'"')+'</a>';
