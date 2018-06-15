27ec0a8499becd1a1b6a07537f06c80cf7817be0
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-14 20:18:55 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -259,7 +259,7 @@ function find(title){
         firebase.database().ref("groups/").orderByChild("info/search").startAt(title.toLowerCase()).endAt(title.toLowerCase()+"\uf8ff").limitToFirst(25).once('value', snapshot => {
             reverseSnapshotOrder(snapshot).forEach(child => {
                 if(i==0){clear("body");}
-                write(child.val().info.group,child.val().info.desc,"group('"+child.key+"');",null,child.val().stats.popularity.toString()+" members");
+                write(child.val().info.group,child.val().stats.popularity.toString()+" members","group('"+child.key+"');");
                 i++;
             });
         });
@@ -336,7 +336,7 @@ function popularity(callback){
             if(child.val().stats.popularity>0){
                 if(i==0){clear("body");}
                 if(child.val().stats.popularity!=0){
-                    write(child.val().info.group,child.val().info.desc,"group('"+child.key+"');",null,child.val().stats.popularity.toString()+" members");
+                    write(child.val().info.group,child.val().stats.popularity.toString()+" members","group('"+child.key+"');");
                 }
                 i++;
             }
@@ -417,7 +417,7 @@ function get(method,path,title,callback){
     });
 }
 
-function write(title,content,link,nav,href,extra){
+function write(title,content,link,nav,href){
     var body="";
     if(link!=null&&nav==null){
         body+='<div class="card" onclick="javascript:'+link+'">';
@@ -448,11 +448,6 @@ function write(title,content,link,nav,href,extra){
            body+='<br />';
        }
     }
-    if(extra!=null){
-       body+=encode(extra).replace(/&amp;quot;/g,'"');
-       body+='<br />';
-    }
-    console.log(extra);
     if(link!=null&&nav!=null){
         body+='<a href="'+link+'">'+encode(nav).replace(/&amp;quot;/g,'"')+'</a>';
     }
