d6c64b8beb329ab340d06bf934d55c68c054bea5
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-25 19:22:30 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -2,6 +2,23 @@ Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
 });
 
+function displayNotification(title,body,icon) {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: body,
+        icon: icon,
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        }
+      };
+      reg.showNotification(title, options);
+    });
+  }
+}
+
 (function() {
     try {
         var $_console$$ = console;
