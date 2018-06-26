30a49cb728f078d2e4b7c63df5e914fa0059e891
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-26 07:55:14 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -1,9 +1,9 @@
 var CACHE_NAME = new Date().getFullYear().toString()+new Date().getMonth().toString()+new Date().getDate().toString()+new Date().getHours().toString()+new Date().getMinutes().toString();
 
-var urlsToCache = [
+var urlsToCache = [/*
   '/apps/gatherapp/',
   '/apps/gatherapp/app.js',
-  '/apps/gatherapp/index.html',
+  '/apps/gatherapp/index.html',*/
   '/apps/gatherapp/manifest.json',
   '/apps/gatherapp/192x192.png',
   '/apps/gatherapp/512x512.png',
