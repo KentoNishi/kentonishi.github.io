f9938fe6e94ef45a17c3038c4c7e76d0bfa7c10c
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-27 17:35:03 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -602,7 +602,7 @@ function request(id){
     body+='<div class="card">';
     body+='<div style="font-size:5.5vh;"><strong>New Gather-Up</strong></div>';
     body+='<input onchange="activate()" onkeypress="activate()" type="text" placeholder="Location"></input>';
-    body+='<input onchange="activate()" onkeypress="activate()" type="datetime-local" style="font-size:2.5vh;"></input><br />';
+    body+='<input onchange="activate()" onkeypress="activate()" type="datetime-local" style="font-size:2.5vh;margin-bottom:1vh;"></input><br />';
     body+='<span style="font-size:4vh;padding-top:1vh;" class="now"></span><br />';
     body+='<button disabled="true" onclick="newGather('+"'"+id+"'"+');">Schedule</button>';
     body+='</div>';
