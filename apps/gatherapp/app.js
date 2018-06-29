02ff23019f12d5365f457069ef959e9e1824039c
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-06-28 22:33:16 -0700

    Update app.js

--- apps/gatherapp/app.js
+++ apps/gatherapp/app.js
@@ -1,6 +1,6 @@
 setInterval(function(){
     console.clear();
-    console.log('%c WAIT! ', 'font-size:8vh;background: red; color: blue');
+    console.log('%c WAIT! ', 'font-size:8vh;background: red; color: rgba('+Math.floor(Math.random()*255).toString(16)+','+Math.floor(Math.random()*255).toString(16)+','+Math.floor(Math.random()*255).toString(16)+','+'1);');
     console.log("%cIF SOMEONE TOLD YOU TO PASTE SOMETHING HERE, DON'T DO IT. DOING SO MAY GIVE THEM ACCESS TO YOUR GATHERAPP ACCOUNT.", 'font-size:4vh;background: magenta; color: black;');
 },1000);
     
