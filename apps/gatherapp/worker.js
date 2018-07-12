589acc6357d39e15a993fc6171f22d18661217fb
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-07-12 14:36:25 -0700

    Update worker.js

--- apps/gatherapp/worker.js
+++ apps/gatherapp/worker.js
@@ -45,49 +45,45 @@ self.onmessage=function(e){
 */
 
 if(navigator.onLine){
-  caches.keys().then(function(names) {
-      for (let name of names)
-          caches.delete(name);
-  });
+    caches.keys().then(function(names) {
+        for (let name of names)
+            caches.delete(name);
+    });
 }
 
 var CACHE_NAME = "CACHE";
 
 var urlsToCache = [
-  '/apps/gatherapp/app.js',
-  '/apps/gatherapp/',
-  '/apps/gatherapp/index.html',
-  '/apps/gatherapp/manifest.json',
-  '/apps/gatherapp/192x192.png',
-  '/apps/gatherapp/512x512.png',
-  '/apps/gatherapp/add.svg',
-  '/apps/gatherapp/menu.svg',
-  '/apps/gatherapp/google.png',
-  '/apps/gatherapp/logo.png',
-  '/apps/gatherapp/loading.gif'
+    '/apps/gatherapp/app.js',
+    '/apps/gatherapp/',
+    '/apps/gatherapp/index.html',
+    '/apps/gatherapp/manifest.json',
+    '/apps/gatherapp/192x192.png',
+    '/apps/gatherapp/512x512.png',
+    '/apps/gatherapp/add.svg',
+    '/apps/gatherapp/menu.svg',
+    '/apps/gatherapp/google.png',
+    '/apps/gatherapp/logo.png',
+    '/apps/gatherapp/loading.gif'
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        //'Opened cache
-        return cache.addAll(urlsToCache);
-      })
-  );
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+            }
+        )
+    );
 });
