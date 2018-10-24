b104f23cfb1b355ae91c6f51eab4de9e33ddff22
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-23 22:04:04 -0700

    Update app.js

--- projects/neuralnet/app.js
+++ projects/neuralnet/app.js
@@ -1,1 +1,17 @@
-
+var layers=[];
+var data=[];
+for(var i=0;i<100;i++){
+    var out=Math.round(Math.random());
+    data.push({in:[out,Math.round(Math.random()),Math.round(Math.random())],out:out});
+}
+for(var i=0;i<4;i++){
+	layers.push([]);
+}
+for(var i=0;i<layers.length;i++){
+	for(var j=0;j<3;j++){
+		layers[i].push({w:Math.random(),b:Math.random()});
+    }
+}
+function sigmoid(t) {
+    return 1/(1+Math.pow(Math.E, -t));
+}
