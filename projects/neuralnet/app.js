ab02f70e68d054060a011830b080a3805738d7f9
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-23 22:28:56 -0700

    Update app.js

--- projects/neuralnet/app.js
+++ projects/neuralnet/app.js
@@ -1,17 +1,28 @@
 var layers=[];
-var data=[];
+var input=[];
+var output=[];
 for(var i=0;i<100;i++){
     var out=Math.round(Math.random());
-    data.push({in:[out,Math.round(Math.random()),Math.round(Math.random())],out:out});
+    input.push({in:[out,Math.round(Math.random()),Math.round(Math.random())],out:out});
 }
-for(var i=0;i<4;i++){
+for(var i=0;i<2;i++){
 	layers.push([]);
 }
 for(var i=0;i<layers.length;i++){
 	for(var j=0;j<3;j++){
-		layers[i].push(i!=layers.length-1?({w:Math.random(),b:Math.random()}):({}));
+		layers[i].push({w:Math.random(),b:Math.random()});
     }
 }
+input.forEach(data=>{
+	for(var i=0;i<layers.length;i++){
+		for(var j=0;j<layers[i].length;j++){
+			if(i==0){
+				layers[i][j].v=data.in[j];
+            }
+        }
+    }
+});
+console.log(layers);
 function sigmoid(t) {
     return 1/(1+Math.pow(Math.E, -t));
 }
