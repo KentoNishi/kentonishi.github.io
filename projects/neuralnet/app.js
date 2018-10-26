617ad8d53f3f735343d37d13314251cdeb7f1211
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-26 16:01:46 -0700

    Update app.js

--- projects/neuralnet/app.js
+++ projects/neuralnet/app.js
@@ -1,4 +1,5 @@
 (function neuralNet(input){
+    var layers=[];
 	var samples=[];
 	for(var i=0;i<100;i++){
 		samples.push({in:[]});
@@ -7,14 +8,28 @@
         }
 		samples[i].out=samples[i].in[0];
     }
+    for(var i=0;i<input.layers;i++){
+        layers.push([]);
+    }
 	samples.forEach(sample=>{
-        var layers=[];
         for(var i=0;i<input.layers;i++){
-            layers.push([]);
             for(var j=0;j<input.layers;j++){
-                layers[i].push({w:Math.random(),b:Math.random(),v:i==0?sample.in[j]:0});
+                layers[i][j]=({w:Math.random(),b:Math.random(),v:i==0?sample.in[j]:0});
             }
         }
-		console.log(sample);
+    	for(var i=0;i<layers.length;i++){
+			if(i!=0){
+				for(var j=0;j<layers[i].length;j++){
+					for(var k=0;k<layers[i-1].length;k++){
+						layers[i][j].v+=layers[i-1][k].w*layers[i-1][k].v;
+                    }
+					layers[i][j].v=sigmoid(layers[i][j].v+layers[i][j].b);
+                }
+            }
+        }
     });
 })({layers:3,neurons:3});
+
+function sigmoid(t) {
+    return 1/(1+Math.pow(Math.E, -t));
+}
