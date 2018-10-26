d0e7c2e5e3f7b5ff666e2545028aac6baa26a274
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-26 13:45:02 -0700

    Update app.js

--- projects/neuralnet/app.js
+++ projects/neuralnet/app.js
@@ -1,10 +1,20 @@
 (function neuralNet(input){
-	var layers=[];
-	for(var i=0;i<input.layers;i++){
-		layers.push([]);
-		for(var j=0;j<input.layers;j++){
-			layers[i].push({w:Math.random(),b:Math.random(),v:0});
+	var samples=[];
+	for(var i=0;i<100;i++){
+		samples.push({in:[]});
+		for(var j=0;j<input.neurons;j++){
+			samples[i].in.push(Math.round(Math.random()));
         }
+		samples[i].out=samples[i].in[0];
     }
-	console.log(layers);
+	samples.forEach(sample=>{
+        var layers=[];
+        for(var i=0;i<input.layers;i++){
+            layers.push([]);
+            for(var j=0;j<input.layers;j++){
+                layers[i].push({w:Math.random(),b:Math.random(),v:i==0?sample.in[j]:0});
+            }
+        }
+		console.log(sample);
+    });
 })({layers:3,neurons:3});
