da7aab7e0b2fd9f73e49ce9ade9a137df13e1efb
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-28 12:54:10 -0700

    Update app.js

--- projects/neuralnet/app.js
+++ projects/neuralnet/app.js
@@ -3,40 +3,46 @@
     var layers=[];
 	var samples=[];
 	for(var i=0;i<100;i++){
-		samples.push({in:[]});
-		for(var j=0;j<input.neurons;j++){
-			samples[i].in.push(Math.round(Math.random()*(input.neurons-1)));
+        samples.push([]);
+		for(var k=0;k<100;k++){
+            samples[i].push({in:[]});
+            for(var j=0;j<input.neurons;j++){
+                samples[i][k].in.push(Math.round(Math.random()*(input.neurons-1)));
+            }
+            samples[i][k].out=samples[i][k].in[0];
         }
-		samples[i].out=samples[i].in[0];
     }
     for(var i=0;i<input.layers;i++){
         layers.push([]);
     }
-	samples.forEach(sample=>{
-        for(var i=0;i<input.layers;i++){
-            for(var j=0;j<input.neurons;j++){
-                layers[i][j]=({w:Math.random(),/*b:Math.random(),*/v:i==0?sample.in[j]:0});
+    samples.forEach(batch=>{
+        batch.forEach(sample=>{
+            for(var i=0;i<input.layers;i++){
+                for(var j=0;j<input.neurons;j++){
+                    layers[i][j]=({w:Math.random(),/*b:Math.random(),*/v:i==0?sample.in[j]:0});
+                }
             }
-        }
-    	for(var i=0;i<layers.length;i++){
-			if(i!=0){
-				for(var j=0;j<layers[i].length;j++){
-					for(var k=0;k<layers[i-1].length;k++){
-						layers[i][j].v+=layers[i-1][k].w*layers[i-1][k].v;
-                    }
-					layers[i][j].v=sigmoid(layers[i][j].v/*+layers[i][j].b*/);
-                } 
+            for(var i=0;i<layers.length;i++){
+                if(i!=0){
+                    for(var j=0;j<layers[i].length;j++){
+                        for(var k=0;k<layers[i-1].length;k++){
+                            layers[i][j].v+=layers[i-1][k].w*layers[i-1][k].v;
+                        }
+                        layers[i][j].v=sigmoid(layers[i][j].v/*+layers[i][j].b*/);
+                    } 
+                }
             }
-        }
-//		cost(layers[layers.length-1],sample.out);
-//		console.log(cost(layers[layers.length-1],sample.out),layers);
-    	for(var i=layers.length-1;i>0;i--){
-			if(i!=1){
-				for(var j=0;j<layers[i].length;j++){
-					console.log(layers[i][j]);
-                } 
-            }
-        }
+    //		cost(layers[layers.length-1],sample.out);
+    //		console.log(cost(layers[layers.length-1],sample.out),layers);
+/*
+            for(var i=layers.length-1;i>0;i--){
+                if(i!=1){
+                    for(var j=0;j<layers[i].length;j++){
+                        console.log(layers[i][j]);
+                    } 
+                }
+            }*/
+        });
     });
 })({layers:3,neurons:3});
 
