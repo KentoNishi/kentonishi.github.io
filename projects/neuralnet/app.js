fdbe84efe68cf56dd2c8ff487cd8bfa11642c1a2
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-28 18:03:20 -0700

    Update app.js

--- projects/neuralnet/app.js
+++ projects/neuralnet/app.js
@@ -4,11 +4,32 @@
 		samples.push([]);
 		for(var j=0;j<input.samples;j++){
 			var out=Math.random();
-			samples[i].push({in:[Math.round(out),Math.round(Math.random()),Math.round(Math.random())],out:Math.round(out)});
+			var inp=[Math.round(out)];
+			for(var k=0;k<input.layers[0]-1;k++){
+				inp.push(Math.round(Math.random()));
+            }
+			samples[i].push({in:inp,out:Math.round(out)});
         }
     }
+	var layers=[];
+	var connections=[[]];
+	for(var i=0;i<input.layers.length;i++){
+		layers.push([]);
+		for(var j=0;j<input.layers[i];j++){
+			layers[i].push({a:0,b:Math.random()*2-1});
+        }
+    }
+	samples.forEach(batch=>{
+		batch.forEach(sample=>{
+			sample.in.forEach(trigger=>{
+				
+            });
+        });
+    });
     function sigmoid(t) {
         return 1/(1+Math.pow(Math.E, -t));
     }
-	console.log("Samples: ",samples,"\n");
-})({batches:10,samples:10});
+	console.log("Samples: ",samples,"\n",
+				"Layers: ",layers,"\n",
+				"Connections: ",connections,"\n");
+})({batches:10,samples:10,layers:[3,3,3,1]});
