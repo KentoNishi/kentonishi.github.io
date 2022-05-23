3e6fd62f2b6a43e02cb83d7bd1c1680a84615a5d
KentoNishi <KentoNishi@users.noreply.github.com>
2022-05-23 06:49:48 +0000

    deploy: 33c5c234c663ecc65ff88eb2590ef78d68a1c56c

--- assets/Blog.f07913f1.js
+++
@@ -1,3 +0,0 @@
-import{S as n,i as r,s as m,P as c,c as i,n as a,f as p,Q as h}from"./index.e649e289.js";const f=`<h1>Placeholder Post</h1>
-<p>Posts will show up here!</p>
-`;function u(l){let t,e;return{c(){t=new h,e=c(),t.a=e},m(s,o){t.m(f,s,o),i(s,e,o)},p:a,i:a,o:a,d(s){s&&p(e),s&&t.d()}}}class _ extends n{constructor(t){super(),r(this,t,null,u,m,{})}}export{_ as default};
