ab29772e7374bc40316e32202ac7f4991a357685
Kento Nishi <kento24gs@outlook.com>
2022-12-08 14:50:18 -0500

    extensions blog post

--- src/views/Blog.svelte
+++ src/views/Blog.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import Cards from '../components/Cards.svelte';
   import Card from '../components/Card.svelte';
-  import { exioZoomInAnimation } from 'exio';
+  import '../css/page.css';
+  import { exioButton, exioZoomInAnimation } from 'exio';
   import { _ } from 'svelte-i18n';
-  import { attributes as placeholderAttrs } from '../posts/placeholder.md';
+  // import { attributes as placeholderAttrs } from '../posts/placeholder.md';
 </script>
 
 <div use:exioZoomInAnimation style="transform-origin: top center;">
@@ -12,8 +13,24 @@
       <svelte:fragment slot="title">{$_('entries.blog.title')}</svelte:fragment>
     </Card>
     <Card>
+      <svelte:fragment slot="title">
+        Browser Extension Standards: How Google Monopolized and Exploited the
+        Web Browser Industry
+      </svelte:fragment>
+      <svelte:fragment slot="content">
+        <p>Harvard Science @ Work Fall 2022 Term Paper</p>
+        <div class="buttons">
+          <a href="/extensions.pdf" target="_blank">
+            <button use:exioButton class="popup">
+              {$_('entries.blog.pdf')}
+            </button>
+          </a>
+        </div>
+      </svelte:fragment>
+    </Card>
+    <!-- <Card>
       <div slot="title">{@html placeholderAttrs.title}</div>
       <div slot="content">{@html placeholderAttrs.description}</div>
-    </Card>
+    </Card> -->
   </Cards>
 </div>
